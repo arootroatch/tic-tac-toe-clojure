5943a1c6e6e4ae54c3f3c3e756eae661e1f362d2
Alex Root-Roatch <arootroatch@gmail.com>
2024-06-05 22:24:16 -0500

    added error checks for incomplete board. Created helper functions won? and get-winner. updated score function to detect and return the winner as well as detect when board is a tie.

--- spec/tic_tac_toe/core_spec.clj
+++ spec/tic_tac_toe/core_spec.clj
@@ -12,16 +12,20 @@
     (it "throws error if board is less than 9"
       (should= "Please enter a valid board" (score [])))
 
-    (it "is in progress if board is not full"
+    (it "is in progress if board is not full and has no winner"
       (should= :in-progress (score [:x :o :x :o :o "" "" "" ""])))
 
-    #_(it "returns winner if three in a row"
-        (should= :x (score [:x :x :x "" "" "" "" "" ""]))))
+    (it "returns winner if three in a row"
+      (should= :x (score [:x :x :x "" "" "" "" "" ""])))
+
+    (it "is a tie if board is full and has no winner"
+      (should= "It's a tie!" (score [:x :o :x :x :x :o :o :x :o]))))
 
 
-  (context "->triplets"
+  (context "->paths"
     (it "throws error if board is less than 9"
-      (should= "Please enter a valid board" (score [])))
+      (should= "Please enter a valid board" (->paths []))
+      (should= "Please enter a valid board" (->paths ["" "" ""])))
 
     (it "separates board into winning paths"
       (should= (repeat 8 (repeat 3 "")) (->paths (repeat 9 "")))
--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -2,20 +2,35 @@
 
 (defn ->paths [board]
   "Top, middle, bottom, left, middle, right, l-r, r-l"
-  (concat (partition 3 board)
-          (list
-            (take-nth 3 board)
-            (take-nth 3 (drop 1 board))
-            (take-nth 3 (drop 2 board))
-            (take-nth 4 board)
-            (take-nth 2 (drop-last (drop 2 board))))))
+  (if (< (count board) 9)
+    "Please enter a valid board"
+    (concat (partition 3 board)
+            (list
+              (take-nth 3 board)
+              (take-nth 3 (drop 1 board))
+              (take-nth 3 (drop 2 board))
+              (take-nth 4 board)
+              (take-nth 2 (drop-last (drop 2 board)))))))
 
+
+(defn won? [board]
+  (some (or #(every? #{:x} %) #(every? #{:o} %)) (->paths board)))
+
+(defn get-winner [board]
+  (first (flatten (filter (or #(every? #{:x} %) #(every? #{:o} %)) (->paths board)))))
 
 (defn score [board]
   (cond
-    (empty? board) "Please enter a valid board"
-    (not (every? #{:x :o} board)) :in-progress))
+    (< (count board) 9) "Please enter a valid board"
+    (and (not (won? board)) (not (every? #{:x :o} board))) :in-progress
+    (and (not (won? board)) (every? #{:x :o} board)) "It's a tie!"
+    (won? board) (get-winner board)))
+
 
 (defn -main
   [& args]
   (println "Hello World"))
+
+;sorts into paths every time it check for win and then again to get win
+;sorts into paths and iterates through just to check winner and again to find winner
+;state booleans? game board data in state?
