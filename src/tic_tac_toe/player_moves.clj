39d413e915ef9b444cf27b3e3667fa849e50dd89
Alex Root-Roatch <arootroatch@gmail.com>
2024-06-06 13:55:35 -0500

    edit set-move function-was only accepting input of 1 and not 2-9

--- src/tic_tac_toe/player_moves.clj
+++ src/tic_tac_toe/player_moves.clj
@@ -2,6 +2,6 @@
   (:require [tic-tac-toe.utils :refer [throw-invalid-move-error]]))
 
 (defn set-move [move board player]
-  (if (not= move (or 1 2 3 4 5 6 7 8 9))
-    (throw-invalid-move-error)
-    (assoc board (dec move) player)))
+  (if (some #(= move %) [1 2 3 4 5 6 7 8 9])
+    (assoc board (dec move) player)
+    (throw-invalid-move-error)))
