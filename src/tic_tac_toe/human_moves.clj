a8ecb876d740a0600f04c1b02e4f566e07096e98
Alex Root-Roatch <arootroatch@gmail.com>
2024-06-20 16:10:07 -0500

    CvC - update ui prompts and prints. minimax currently only plays as O

--- src/tic_tac_toe/human_moves.clj
+++ src/tic_tac_toe/human_moves.clj
@@ -2,7 +2,7 @@
   (:require [tic-tac-toe.ui :as print-utils]
             [tic-tac-toe.player :as player]))
 
-(defmethod player/take-turn 0 [{:keys [board player mode]}]
+(defmethod player/take-turn :default [{:keys [board player mode]}]
   (print-utils/prompt-user-for-move player mode)
   (loop [move (print-utils/read-user-input)]
     (if (and (number? move) (number? (get board (dec move))))
