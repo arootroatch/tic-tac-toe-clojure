dbc7506a79eb1df1395ce45c7f6c5cce476d989b
Alex Root-Roatch <arootroatch@gmail.com>
2024-06-20 17:01:33 -0500

    minimax now plays as either token. updated both minimax and eval-board and added testing for both player scenarios

--- spec/tic_tac_toe/ai_spec/minimax_spec.clj
+++ spec/tic_tac_toe/ai_spec/minimax_spec.clj
@@ -4,7 +4,7 @@
             [clojure.math.combinatorics :as combo]
             [tic-tac-toe.eval-board :as eval-board]))
 
-(defn possible-combo? [moves]
+(defn- possible-combo? [moves]
   (or
     (and (= 1 (first moves)) (not (some #{5} moves)))
     (and (= 2 (first moves)) (not (some #{1} moves)))
@@ -16,11 +16,11 @@
     (and (= 8 (first moves)) (not (some #{2} moves)))
     (and (= 9 (first moves)) (not (some #{5} moves)))))
 
-(defn play-minimax-turn [board player]
+(defn- play-minimax-turn [board player]
   (let [move (find-best-move board player)]
     (assoc board (dec move) player)))
 
-(defn play-sim-game [coll]
+(defn- play-sim-game [coll]
   (loop [moves coll
          board [1 2 3 4 5 6 7 8 9]
          player (cycle [:x :o])]
@@ -40,31 +40,76 @@
              (if (= "X wins!" (play-sim-game (first all-move-combos))) true false)
              (inc game-count)))))
 
+(defn- play-ai-game []
+  (loop [player (cycle [:x :o])
+         board [1 2 3 4 5 6 7 8 9]]
+    (if (not= (eval-board/score board) :in-progress)
+      (eval-board/score board)
+      (recur (rest player) (play-minimax-turn board (first player))))))
+
+(defn- always-ties? []
+  (loop [ties? true
+         games (range 1 1001)]
+    (if (or (empty? games) (not ties?))
+      ties?
+      (recur (if (= "It's a tie!" (play-ai-game)) true false) (rest games)))))
+
 (describe "computer opponent - is maximizer"
-  (it "finds move if next move is a win"
-    (should= 2 (find-best-move [:o 2 :o :x :x :o :x 8 :x] :o))
-    (should= 2 (find-best-move [:o 2 :o :x :x 6 :x 8 9] :o))
-    (should= 4 (find-best-move [1 :x :x 4 :o :o :o :x :x] :o))
-    (should= 3 (find-best-move [1 2 3 :x :x :o 7 :x :o] :o)))
 
-  (it "finds move if next move is a block"
-    (should= 4 (find-best-move [1 2 :o 4 :x :x 7 8 9] :o))
-    (should= 1 (find-best-move [1 2 :o 4 :x 6 7 8 :x] :o))
-    (should= 9 (find-best-move [:x :o :x :o :o :x 7 :x 9] :o))
-    (should= 4 (find-best-move [:x :o :x 4 :o 6 :x :x :o] :o)))
+  (context "plays as O"
+    (it "finds move if next move is a win"
+      (should= 2 (find-best-move [:o 2 :o :x :x :o :x 8 :x] :o))
+      (should= 2 (find-best-move [:o 2 :o :x :x 6 :x 8 9] :o))
+      (should= 4 (find-best-move [1 :x :x 4 :o :o :o :x :x] :o))
+      (should= 3 (find-best-move [1 2 3 :x :x :o 7 :x :o] :o)))
 
-  (it "finds move with most potential to win if not blocked"
-    (should= 5 (find-best-move [:o :x :o :x 5 6 7 8 :x] :o)))
+    (it "finds move if next move is a block"
+      (should= 4 (find-best-move [1 2 :o 4 :x :x 7 8 9] :o))
+      (should= 1 (find-best-move [1 2 :o 4 :x 6 7 8 :x] :o))
+      (should= 9 (find-best-move [:x :o :x :o :o :x 7 :x 9] :o))
+      (should= 4 (find-best-move [:x :o :x 4 :o 6 :x :x :o] :o)))
+
+    (it "finds move with most potential to win if not blocked"
+      (should= 5 (find-best-move [:o :x :o :x 5 6 7 8 :x] :o)))
+
+    (tags :slow)
+    (it "plays center if first move is a corner"
+      (should= 5 (find-best-move [:x 2 3 4 5 6 7 8 9] :o))
+      (should= 5 (find-best-move [1 2 :x 4 5 6 7 8 9] :o))
+      (should= 5 (find-best-move [1 2 3 4 5 6 :x 8 9] :o))
+      (should= 5 (find-best-move [1 2 3 4 5 6 7 8 :x] :o)))
+    )
+
+
+  (context "plays as X"
+    (it "finds move if next move is a win"
+      (should= 2 (find-best-move [:x 2 :x :o :o :x :o 8 :o] :x))
+      (should= 2 (find-best-move [:x 2 :x :o :o 6 :o 8 9] :x))
+      (should= 4 (find-best-move [1 :o :o 4 :x :x :x :o :o] :x))
+      (should= 3 (find-best-move [1 2 3 :o :o :x 7 :o :x] :x)))
+
+    (it "finds move if next move is a block"
+      (should= 4 (find-best-move [1 2 :x 4 :o :o 7 8 9] :x))
+      (should= 1 (find-best-move [1 2 :x 4 :o 6 7 8 :o] :x))
+      (should= 9 (find-best-move [:o :x :o :x :x :o 7 :o 9] :x))
+      (should= 4 (find-best-move [:o :x :o 4 :x 6 :o :o :x] :x)))
+
+    (it "finds move with most potential to win if not blocked"
+      (should= 5 (find-best-move [:x :o :x :o 5 6 7 8 :o] :x)))
+
+    (tags :slow)
+    (it "plays center if first move is a corner"
+      (should= 5 (find-best-move [:o 2 3 4 5 6 7 8 9] :x))
+      (should= 5 (find-best-move [1 2 :o 4 5 6 7 8 9] :x))
+      (should= 5 (find-best-move [1 2 3 4 5 6 :o 8 9] :x))
+      (should= 5 (find-best-move [1 2 3 4 5 6 7 8 :o] :x)))
+    )
 
   (tags :slow)
-  (it "plays center if first move is a corner"
-    (should= 5 (find-best-move [:x 2 3 4 5 6 7 8 9] :o))
-    (should= 5 (find-best-move [1 2 :x 4 5 6 7 8 9] :o))
-    (should= 5 (find-best-move [1 2 3 4 5 6 :x 8 9] :o))
-    (should= 5 (find-best-move [1 2 3 4 5 6 7 8 :x] :o)))
+  (it "wins or ties every possible game against human player"
+    (should= true (unbeatable?)))
 
-  (tags :slow)
-  (it "wins or ties every possible game"
-    (should= true (unbeatable?))))
+  (it "minimax vs minimax always ties"
+    (should= true (always-ties?))))
 
 
--- src/tic_tac_toe/ai/minimax.clj
+++ src/tic_tac_toe/ai/minimax.clj
@@ -3,6 +3,9 @@
 
 (declare minimax)
 
+(defn- switch-player [player]
+  (if (= player :x) :o :x))
+
 (defn- min-move [board player depth]
   (loop [available (filter number? board)
          best-score 1000]
@@ -10,7 +13,7 @@
       best-score
       (let [move (first available)
             new-board (assoc board (dec move) player)
-            score (minimax new-board true (inc depth))]
+            score (minimax new-board true (inc depth) (switch-player player))]
         (recur (rest available) (min best-score score))))))
 
 (def min-move (memoize min-move))
@@ -22,17 +25,18 @@
       best-score
       (let [move (first available)
             new-board (assoc board (dec move) player)
-            score (minimax new-board false (inc depth))]
+            score (minimax new-board false (inc depth) (switch-player player))]
         (recur (rest available) (max best-score score))))))
 
 (def max-move (memoize max-move))
 
-(defn- minimax [board max? depth]
-  (let [game-state (evaluate-board board depth)]
+(defn- minimax [board max? depth player]
+  (let [maximizer (if max? player (switch-player player))
+        game-state (evaluate-board board depth maximizer)]
     (cond
       (not= :in-progress game-state) game-state
-      max? (max-move board :o depth)
-      :else (min-move board :x depth))))
+      max? (max-move board player depth)
+      :else (min-move board player depth))))
 
 (defn- find-best-move [board player]
   (loop [available (filter number? board)
@@ -42,7 +46,7 @@
       best-move
       (let [move (first available)
             new-board (assoc board (dec move) player)
-            score (minimax new-board false 0)]
+            score (minimax new-board false 0 (switch-player player))]
         (if (> score best-score)
           (recur (rest available) move score)
           (recur (rest available) best-move best-score))))))
