e991f3f0c5928f4fdb41cf497e90c7a4f67e3384
Alex Root-Roatch <arootroatch@gmail.com>
2024-07-03 19:52:41 -0500

    make main multi-arity so 'lein run' still works

--- src/tic_tac_toe/main.clj
+++ src/tic_tac_toe/main.clj
@@ -1,15 +1,14 @@
 (ns tic-tac-toe.main
   (:require [tic-tac-toe.bot-moves]
-            [tic-tac-toe.human-moves]
             [tic-tac-toe.eval-board :refer [score]]
-            [tic-tac-toe.player :refer [take-turn player-sequence]]
-            [tic-tac-toe.ui.get-selection :refer [get-selection initial-3x3-board initial-4x4-board initial-3x3x3-board]]
+            [tic-tac-toe.human-moves]
+            [tic-tac-toe.player :refer [player-sequence take-turn]]
+            [tic-tac-toe.ui.get-selection :refer [get-selection initial-3x3-board initial-3x3x3-board initial-4x4-board]]
             [tic-tac-toe.ui.print-utils :as print-utils]))
 
-(defn -main [&args]
-  (let [gui (if (= &args "gui") (do (load-file "src/tic_tac_toe/gui/core.clj") 1) nil)
-        mode (get-selection {:option :mode :gui gui})
-        board-selection (get-selection {:option :board :gui ui})
+(defn- play-loop [gui]
+  (let [mode (get-selection {:option :mode :gui gui})
+        board-selection (get-selection {:option :board :gui gui})
         first-ai-level (if (= 1 mode) nil (get-selection {:option :level :ai 1 :mode mode :board board-selection :gui gui}))
         second-ai-level (if (= 4 mode) (get-selection {:option :level :ai 2 :mode mode :gui gui}) nil)
         game-options {:mode mode :first-ai-level first-ai-level :second-ai-level second-ai-level :gui gui}]
@@ -20,4 +19,10 @@
         (println (score board))
         (recur (take-turn (assoc game-options :board board :player (first player))) (rest player))))))
 
-
+(defn -main
+  ([]
+   (play-loop nil))
+  ([&args]
+   (let [gui (if (= &args "gui") (do (load-file "src/tic_tac_toe/gui/core.clj") 1) nil)]
+     (play-loop gui)))
+  )
