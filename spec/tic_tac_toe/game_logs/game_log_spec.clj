de0953f2cef705db4cace023dd398b2949d5b166
Alex Root-Roatch <arootroatch@gmail.com>
2024-07-16 09:32:39 -0500

    log terminal games

--- spec/tic_tac_toe/game_logs/game_log_spec.clj
+++ spec/tic_tac_toe/game_logs/game_log_spec.clj
@@ -3,6 +3,7 @@
             [tic-tac-toe.game_logs.game-log :refer :all]))
 
 (def test-path "spec/tic_tac_toe/game_logs/game-logs-test.edn")
+(def game-ids-test-path "spec/tic_tac_toe/game_logs/game-ids.edn")
 (def empty-test-path "spec/tic_tac_toe/game_logs/game-logs-empty-test.edn")
 
 (describe "logging games"
@@ -37,31 +38,34 @@
     (with-redefs [get-last-in-progress-game (stub :last-game {:return nil})]
       (should= nil (get-last-in-progress-game-id "spec/tic_tac_toe/game_logs/in_progress"))))
 
-  (it "gets last game id"
-    (with-redefs [get-last-in-progress-game-id (stub :get-last-in-progress-game-id {:return nil})]
-      (should= 2 (get-last-game-id test-path "spec/tic_tac_toe/game_logs/in_progress"))))
-
-  (it "includes in progress games when finding last game id"
-      (should= 4 (get-last-game-id test-path "spec/tic_tac_toe/game_logs/in_progress")))
-
   (it "gets new game id"
-    (should= 5 (get-new-game-id test-path "spec/tic_tac_toe/game_logs/in_progress")))
+    (should= 5 (get-new-game-id game-ids-test-path)))
 
   (it "returns 1 if no previous games"
-    (with-redefs [get-last-in-progress-game-id (stub :get-last-in-progress-game-id {:return nil})]
-      (should= 1 (get-new-game-id empty-test-path "spec/tic_tac_toe/game_logs/in_progress"))))
+      (should= 1 (get-new-game-id empty-test-path)))
+
+  (it "adds game id to game id file"
+    (with-redefs [spit (stub :spit)]
+      (log-game-id game-ids-test-path 5)
+      (should-have-invoked :spit {:with ["spec/tic_tac_toe/game_logs/game-ids.edn" "5\n" :append true]})))
 
   (it "gets paths in given directory"
     (should= ["spec/tic_tac_toe/game_logs/game-logs-empty-test.edn"
               "spec/tic_tac_toe/game_logs/in_progress"
               "spec/tic_tac_toe/game_logs/in_progress/game-4.edn"
+              "spec/tic_tac_toe/game_logs/in_progress_empty"
               "spec/tic_tac_toe/game_logs/game-logs-test.edn"
-              "spec/tic_tac_toe/game_logs/game_log_spec.clj"]
+              "spec/tic_tac_toe/game_logs/game_log_spec.clj"
+              "spec/tic_tac_toe/game_logs/game-ids.edn"]
              (get-paths-in-dir "spec/tic_tac_toe/game_logs")))
+
+  (it "creates new filename and path for in progress game"
+    (should= "spec/tic_tac_toe/game_logs/in_progress/game-5.edn"
+             (create-new-filepath "spec/tic_tac_toe/game_logs/in_progress" 5)))
 
   (it "creates in progress game file with game data"
     (with-redefs [spit (stub :spit)]
-      (create-in-progress-game-file test-path "spec/tic_tac_toe/game_logs/in_progress" {:game-state :in-progress})
+      (create-in-progress-game-file "spec/tic_tac_toe/game_logs/in_progress/game-5.edn" {:game-state :in-progress :game-id 5})
       (should-have-invoked :spit {:with ["spec/tic_tac_toe/game_logs/in_progress/game-5.edn"
                                          {:game-state :in-progress :game-id 5}]})))
 
@@ -83,18 +87,8 @@
     (with-redefs [spit (stub :spit)
                   clojure.java.io/delete-file (stub :delete-file)]
       (log-completed-game "spec/tic_tac_toe/game_logs/in_progress/game-4.edn" test-path)
-      (should-have-invoked :spit {:with [test-path
-                                         {:game-id 4
-                                          :game-state :in-progress
-                                          :ui :gui
-                                          :mode 2
-                                          :board [1 2 3 4 5 6 7 8 9]
-                                          :first-ai-level 3
-                                          :second-ai-level nil
-                                          :player :o
-                                          :human? false
-                                          :moves [[1 2 3 4 5 6 7 8 9]
-                                                  [1 2 3 4 :x 6 7 8 9]]}
+      (should-have-invoked :spit {:with ["spec/tic_tac_toe/game_logs/game-logs-test.edn"
+                                         "{:game-id 4, :moves ([1 2 3 4 5 6 7 8 9] [1 2 3 4 :x 6 7 8 9]), :second-ai-level nil, :mode 2, :first-ai-level 3, :game-state :in-progress, :human? false, :ui :gui, :player :o, :board [1 2 3 4 5 6 7 8 9]}\n"
                                          :append true]})))
 
   (it "deletes temp file after logging"
--- src/tic_tac_toe/tui/tui_main.clj
+++ src/tic_tac_toe/tui/tui_main.clj
@@ -1,21 +1,36 @@
 (ns tic-tac-toe.tui.tui-main
   (:require [tic-tac-toe.bot-moves]
             [tic-tac-toe.eval-board :refer [score]]
+            [tic-tac-toe.game_logs.game-log :as game-log]
             [tic-tac-toe.human-moves]
-            [tic-tac-toe.player :refer [player-sequence take-turn]]
+            [tic-tac-toe.launch-user-interface :refer [launch-user-interface]]
+            [tic-tac-toe.player :refer [switch-player take-turn]]
             [tic-tac-toe.tui.get-selection :refer [get-selection initial-3x3-board initial-3x3x3-board initial-4x4-board]]
-            [tic-tac-toe.tui.print-utils :as print-utils]
-            [tic-tac-toe.launch-user-interface :refer [launch-user-interface]]))
+            [tic-tac-toe.tui.print-utils :as print-utils]))
 
-(defmethod launch-user-interface :default [_]
+(defn- get-options []
   (let [mode (get-selection {:option :mode})
         board-selection (get-selection {:option :board})
         first-ai-level (if (= 1 mode) nil (get-selection {:option :level :ai 1 :mode mode :board board-selection}))
         second-ai-level (if (= 4 mode) (get-selection {:option :level :ai 2 :mode mode}) nil)
-        game-options {:mode mode :first-ai-level first-ai-level :second-ai-level second-ai-level}]
-    (loop [board (case board-selection 1 initial-3x3-board 2 initial-4x4-board 3 initial-3x3x3-board)
-           player player-sequence]
-      (print-utils/print-board board)
-      (if (not= (score board) :in-progress)
-        (println (score board))
-        (recur (take-turn (assoc game-options :board board :player (first player))) (rest player))))))
+        board (case board-selection 1 initial-3x3-board 2 initial-4x4-board 3 initial-3x3x3-board)
+        player :x
+        game-id (game-log/get-new-game-id game-log/game-id-path)]
+    {:mode mode :first-ai-level first-ai-level :second-ai-level second-ai-level :board board :player player :game-id game-id}))
+
+(defmethod launch-user-interface :default [_]
+  (let [game-options (get-options)
+        filepath (game-log/create-new-filepath game-log/in-progress-dir-path (:game-id game-options))]
+    (game-log/create-in-progress-game-file filepath game-options)
+    (game-log/log-game-id game-log/game-id-path (:game-id game-options))
+    (loop [game-options game-options]
+      (let [{:keys [board player]} game-options]
+        (print-utils/print-board board)
+        (if (not= (score board) :in-progress)
+          (do
+            (game-log/log-completed-game filepath game-log/logs-path)
+            (println (score board)))
+          (let [new-board (take-turn game-options)]
+            (prn filepath new-board)
+            (game-log/log-move filepath new-board)
+            (recur (assoc game-options :board new-board :player (switch-player player)))))))))
