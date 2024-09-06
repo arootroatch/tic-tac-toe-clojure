4011391d7f00decd59842dabce53740e2bc50dc1
Alex Root-Roatch <arootroatch@gmail.com>
2024-09-06 00:08:38 -0500

    added run-server-w-message fn

--- spec/cljs/tic_tac_toe/main_spec.cljs
+++ spec/cljs/tic_tac_toe/main_spec.cljs
@@ -1,11 +1,10 @@
 (ns tic-tac-toe.main-spec
-  (:require-macros [speclj.core :refer [should= stub it describe before]]
-                   [c3kit.wire.spec-helperc :refer [should-not-select should-select should-have-invoked-ajax-post]])
+  (:require-macros [speclj.core :refer [should= it describe before]]
+                   [c3kit.wire.spec-helperc :refer [should-not-select should-select]])
   (:require
     [speclj.core]
     [c3kit.wire.spec-helper :as wire]
-    [tic-tac-toe.main :as main]
-    ))
+    [tic-tac-toe.main :as main]))
 
 (describe "main"
   (wire/with-root-dom)
@@ -13,6 +12,5 @@
     (wire/render [main/base]))
 
   (it "does stuff"
-    (should= 1 main/one)
     (should-select "#bob")
     (should= "file:///" (wire/href "#bob a"))))
--- src/cljs/tic_tac_toe/main.cljs
+++ src/cljs/tic_tac_toe/main.cljs
@@ -1,16 +1,11 @@
 (ns tic-tac-toe.main
   (:require [reagent.dom :as rdom]
-            [c3kit.wire.js :as wjs])
-  )
+            [c3kit.wire.js :as wjs]))
 
 (defn base []
   [:div
    {:id "bob"}
    [:a {:href "/"}]])
 
-
-(def one 1)
-
 (defn ^:export main []
-  (println "hi from cljs")
   (rdom/render [base] (wjs/element-by-id "app ")))
